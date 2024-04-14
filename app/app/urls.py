5933524dfa82f716f1d87d114e0c7f0e8a16591b
FIFSAK <anuar200572@gmail.com>
2024-04-14 23:52:17 +0500

    finish

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from menu.views import main_view, about_view, contacts_view, menu_view
+from menu.views import about_view, contacts_view, menu_view, home_view, menu_detail_view
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', main_view, name='menus'),
-    path('about/', about_view, name='about'),
-    path('contacts/', contacts_view, name='contacts'),
-    path('menu/', menu_view, name='menu'),
-
+    path('', home_view, name='home'),
+    path('menu/<str:url_name>/', menu_detail_view, name='menu_detail'),
 ]
+
--- app/menu/views.py
+++ app/menu/views.py
@@ -2,11 +2,24 @@ from django.shortcuts import render
 from .models import MenuItem
 
 
-def main_view(request):
-    menus = MenuItem.objects.filter(parent__isnull=True)
-    current_path = request.path
-    context = {'current_path': current_path, 'page_title': 'О нас'}
-    return render(request, 'base.html', context)
+def home_view(request):
+    # Получаем все корневые элементы меню
+    root_menus = MenuItem.objects.filter(parent__isnull=True)
+    return render(request, 'home.html', {'menus': root_menus})
+
+
+# views.py
+
+from django.shortcuts import render, get_object_or_404
+from .models import MenuItem
+
+def menu_detail_view(request, url_name):
+    # Получаем корневой пункт меню по ID
+    root_menu = get_object_or_404(MenuItem, url_name=url_name, parent__isnull=True)
+    # Получаем все дочерние пункты меню для данного корневого элемента
+    menu_items = root_menu.children.all()
+    return render(request, 'menu_detail.html', {'menu_items': menu_items, 'menu': root_menu})
+
 
 
 def about_view(request):
@@ -14,8 +27,8 @@ def about_view(request):
     context = {'current_path': current_path, 'page_title': 'О нас'}
     return render(request, 'base.html', context)
 
-# Повторите это для каждого view, где передаётся context
 
+# Повторите это для каждого view, где передаётся context
 
 
 def contacts_view(request):
