008e3d67c326e9b96fb61a5aefc2e829f614e213
FIFSAK <anuar200572@gmail.com>
2024-04-14 23:59:45 +0500

    finish

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from menu.views import about_view, contacts_view, menu_view, home_view, menu_detail_view
+from menu.views import home_view, menu_detail_view
 
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('admin/', admin.site.urls),
     path('menu/<str:url_name>/', menu_detail_view, name='menu_detail'),
 ]
 
--- app/menu/views.py
+++ app/menu/views.py
@@ -1,43 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import MenuItem
 
 
 def home_view(request):
-    # Получаем все корневые элементы меню
     root_menus = MenuItem.objects.filter(parent__isnull=True)
     return render(request, 'home.html', {'menus': root_menus})
 
 
-# views.py
-
-from django.shortcuts import render, get_object_or_404
-from .models import MenuItem
-
 def menu_detail_view(request, url_name):
-    # Получаем корневой пункт меню по ID
     root_menu = get_object_or_404(MenuItem, url_name=url_name, parent__isnull=True)
-    # Получаем все дочерние пункты меню для данного корневого элемента
     menu_items = root_menu.children.all()
     return render(request, 'menu_detail.html', {'menu_items': menu_items, 'menu': root_menu})
-
-
-
-def about_view(request):
-    current_path = request.path
-    context = {'current_path': current_path, 'page_title': 'О нас'}
-    return render(request, 'base.html', context)
-
-
-# Повторите это для каждого view, где передаётся context
-
-
-def contacts_view(request):
-    current_path = request.path
-    context = {'current_path': current_path, 'page_title': 'Контакты'}
-    return render(request, 'base.html', context)
-
-
-def menu_view(request):
-    current_path = request.path
-    context = {'current_path': current_path, 'page_title': 'Меню'}
-    return render(request, 'base.html', context)
